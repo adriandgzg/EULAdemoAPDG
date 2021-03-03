9d9174ab9280fee0452ea13aaab840cacebb23b6
Adrian Dominguez <adriandgzg@gmail.com>
2021-03-02 23:16:03 -0600

    Update Interactor,Presenter, Router in HomeModule

---
+++ EULAdemoAPDG/Modules/Search/Models/SearchModel.swift
@@ -0,0 +1,8 @@
+//
+//  SearchModel.swift
+//  EULAdemoAPDG
+//
+//  Created by Adrian Dominguez Gómez on 02/03/21.
+//
+
+import Foundation
---
+++ EULAdemoAPDG/Modules/Search/Presenter/SearchPresenter.swift
@@ -0,0 +1,8 @@
+//
+//  SearchPresenter.swift
+//  EULAdemoAPDG
+//
+//  Created by Adrian Dominguez Gómez on 02/03/21.
+//
+
+import Foundation
