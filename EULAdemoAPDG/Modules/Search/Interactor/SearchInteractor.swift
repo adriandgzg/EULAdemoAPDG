3b620ef3c29420251467b9b64cd757776de1959e
Adrian Dominguez <adriandgzg@gmail.com>
2021-03-02 23:38:20 -0600

    add Files for service detail meal (lookup)

--- EULAdemoAPDG/Modules/Search/Interactor/SearchInteractor.swift
+++ EULAdemoAPDG/Modules/Search/Interactor/SearchInteractor.swift
@@ -36,6 +36,9 @@ class InteractorSearch:interactorProtocol {
         ///TODO:Detalle de producto
         dump("irAlDetalle")
         dump(idmeal)
+        
+        
+        
     }
     
     
--- EULAdemoAPDG/Utils/EULAServices/RequestModels/RequestDetailMeal.swift
+++ EULAdemoAPDG/Utils/EULAServices/RequestModels/RequestDetailMeal.swift
@@ -6,3 +6,39 @@
 //
 
 import Foundation
+import Alamofire
+
+
+class RequestLookup: RequestProtocol {
+    var search = ""
+    
+    
+    init(searchString:String){
+        search = searchString
+    }
+    func getQueryParams() -> Parameters? {
+        return ["s":search]
+    }
+    
+    func getParameters() -> Parameters? {
+        return nil
+    }
+    
+    func getUrl() -> String {
+        return stockURL.lookup
+    }
+    
+    func getReplaceKeys() -> Parameters? {
+        return nil
+    }
+    
+    func getHeaders() -> [String : String]? {
+        return nil
+    }
+    
+    func getMethod() -> HTTPMethod {
+        .get
+    }
+    
+
+}
